c51908612d1b48acc20f0f46dbbe675431b9ed5a
kapilchandrawal <kapilchandrawal001@gmail.com>
2022-01-14 13:25:52 +0530

    Made proper indentation

--- join_array_without_inbuilt.rb
+++ join_array_without_inbuilt.rb
@@ -3,9 +3,11 @@
 
 def merge(arr1, arr2)
     arr2.each_with_object(arr1.dup) { |n,arr|
-       arr.insert(arr.bsearch_index { |i| i>n } || arr.size, n) }
+       arr.insert(arr.bsearch_index { |i| 
+            i>n 
+        } || arr.size, n) 
+    }
 end
-arr1 = [5,8,9,11]
-arr2 = [4,6,7,12,13]
+arr1 = [5, 8, 9, 11]
+arr2 = [4, 6, 7, 12, 13]
 print merge(arr1, arr2)
-  
