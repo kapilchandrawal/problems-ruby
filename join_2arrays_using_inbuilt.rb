ce73b5aeb22ff8bb44ea6bccf86165ec539638f4
Kapil Chandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2022-01-24 08:29:53 +0530

    Ruby basic problems review (#1)
    * Print numbers
    
    * Print characters occurence in array
    
    * Join arrays without using inbuilt function
    
    * Join arrays using inbuilt function
    
    * Map method for doubling numbers
    
    * Map method for doubling numbers first problem
    
    * Read from csv file
    
    * Changed filenames
    
    * Made proper indentation
    
    * Added 2 spaces indentation and required changes
    
    * Line spacing
    
    * Removed .json file

---
+++ join_2arrays_using_inbuilt.rb
@@ -0,0 +1,8 @@
+# Join 2 arrays using inbuilt functions.
+
+arr1 = %w[Hello, world]
+
+arr2 = %w[Hi, new-world]
+
+# print arr1.concat arr2
+print arr1 += arr2
---
+++ join_array_without_inbuilt.rb
@@ -0,0 +1,13 @@
+
+# Join 2 arrays without using inbuilt functions.
+
+def merge(arr1, arr2)
+	arr2.each_with_object(arr1.dup) do |n, arr|
+		arr.insert(arr.bsearch_index do |i| 
+			i > n 
+		end || arr.size, n) 
+	end
+end
+arr1 = [5, 8, 9, 11]
+arr2 = [4, 6, 7, 12, 13]
+print merge(arr1, arr2)
